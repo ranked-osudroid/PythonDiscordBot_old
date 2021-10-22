3be439353b614356b8e46bb8c8aadd1847e88041
friend1226 <friendcha7749@gmail.com>
2021-10-22 20:30:41 +0900

    [add] fixca server ResponseManager

---
+++ fixca.py
@@ -0,0 +1,26 @@
+from friend_import import *
+
+class RequestManager:
+    BASEURL = "http://ranked-osudroid.kro.kr/api/"
+
+    def __init__(self, bot):
+        self.bot = bot
+        if bot is not None:
+            self.session = bot.session
+
+    async def post(self, url, data):
+        async with self.session.post(BASEURL+url, data=data) as res:
+            if res.status != 200:
+                return f'POST {url} failed.', res
+            if (resdata := await res.json(encoding='utf-8'))['status'] == 'failed':
+                return f'POST {url} had had occured', res
+            return resdata
+
+    async def get(self, url, data):
+        async with self.session.get(BASEURL+url, data=data) as res:
+            if res.status != 200:
+                return f'POST {url} failed.', res
+            if (resdata := await res.json(encoding='utf-8'))['status'] == 'failed':
+                return f'POST {url} had had occured', res
+            return resdata
+
